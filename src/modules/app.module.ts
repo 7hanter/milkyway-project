e7e45b897810ec4180ad3329ba5d02ae397c74a6
f.moscoso <f.moscoso@getaclub.io>
2021-03-06 20:18:23 -0500

    Mapbox component and service added

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -3,16 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from '@modules/app-routing.module';
 import { AppComponent } from '@components/app/app.component';
-
+import { MapComponent } from '@components/map/map.component';
+import { MapService } from '@services/map.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ MapService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
