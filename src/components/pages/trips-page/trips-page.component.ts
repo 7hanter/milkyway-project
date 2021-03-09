245b92628a074da0f358b46c76d6058b29631a50
f.moscoso <f.moscoso@getaclub.io>
2021-03-09 03:02:13 -0500

    Models added, list components & subcomponents implemented and connections approach

--- src/components/pages/trips-page/trips-page.component.ts
+++ src/components/pages/trips-page/trips-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { TripLocation } from '@models/location.model';
+import { MOCK_TRIPS, Trip } from '@models/trip.model';
 
 @Component({
   selector: 'milkyway-trips-page',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./trips-page.component.scss']
 })
 export class TripsPageComponent implements OnInit {
-
+  public tripList: Array<Trip> = MOCK_TRIPS;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public setClickedTrip(trip: TripLocation): void {
+    console.log(trip);
+  }
 }
